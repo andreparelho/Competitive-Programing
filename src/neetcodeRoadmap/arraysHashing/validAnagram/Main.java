82d5a9436368e88afe68f3abbf8bb3ca62dc47f2
Andre <parelhoneves.andre@gmail.com>
2024-01-18 00:12:03 -0300

    update class

--- src/neetcodeRoadmap/arraysHashing/validAnagram/Main.java
+++ src/neetcodeRoadmap/arraysHashing/validAnagram/Main.java
@@ -1,8 +1,6 @@
 package neetcodeRoadmap.arraysHashing.validAnagram;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
 public class Main {
