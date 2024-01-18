983770adb874a94faec0845d782d0ca852c71083
Andre <parelhoneves.andre@gmail.com>
2024-01-18 00:11:20 -0300

    update packge name

---
+++ src/neetcodeRoadmap/arraysHashing/validAnagram/Main.java
@@ -0,0 +1,41 @@
+package neetcodeRoadmap.arraysHashing.validAnagram;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Main {
+    public boolean isAnagram(String s, String t) {
+        ArrayList<String> listS = new ArrayList<>();
+        ArrayList<String> listT = new ArrayList<>();
+
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+
+        for (int i = 0; i < charS.length; i++) {
+            listS.add(String.valueOf(charS[i]));
+        }
+
+        for (int i = 0; i < charT.length; i++) {
+            listT.add(String.valueOf(charT[i]));
+        }
+
+        Collections.sort(listS);
+        Collections.sort(listT);
+
+        if (listS.equals(listT)){
+            return true;
+        }
+
+        return false;
+    }
+
+//    Test
+    public static void main(String[] args) {
+        String s = "rat";
+        String ss = "car";
+
+        //System.out.println(isAnagram(s, ss));
+    }
+}
