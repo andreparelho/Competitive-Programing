8dce54ab91c5d0097d23d8241448c51b4fa3e10d
Andre <parelhoneves.andre@gmail.com>
2024-01-17 23:41:51 -0300

    feat: create new exercise, neetcode roadmap

---
+++ src/neetcodeRoadmap/arraysHashing/ex01ContainsDuplicate/ex001ContainsDuplicate.java
@@ -0,0 +1,23 @@
+package neetcodeRoadmap.arraysHashing.ex01ContainsDuplicate;
+
+import java.util.ArrayList;
+
+public class ex001ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int num : nums) {
+            if (arrayList.contains(num)){
+                return true;
+            }
+            arrayList.add(num);
+        }
+        return false;
+    }
+
+//    Test
+    public static void main(String[] args) {
+        int[] ints = {1,3,4,6,1};
+        //System.out.println(containsDuplicate(ints));
+    }
+}
